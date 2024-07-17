a9120b80647f0fcba86a9bad9e1a9ef3559dffdd
mishanya <homakhaker@gmail.com>
2024-07-17 17:49:47 +0200

    Changed check token to return a user

--- src/front_api/gql_query/login_query.rs
+++ src/front_api/gql_query/login_query.rs
@@ -1,8 +1,11 @@
+use std::future::Future;
 use async_graphql::{Context, FieldResult, Object};
 use log::error;
-use sqlx::PgPool;
+use sqlx::{Error, PgPool};
 use crate::front_api::gql_query::LoginQuery;
 use crate::front_api::{gql_auth, utils};
+use crate::front_api::gql_models::user_gql_model::GqlUser;
+use crate::psql::user_psql_model::UserModel;
 use crate::services;
 
 #[Object(extends)]
@@ -11,7 +14,7 @@ impl LoginQuery {
         &self,
         ctx: &Context<'_>,
         token: String,
-    ) -> FieldResult<bool> {
+    ) -> FieldResult<GqlUser> {
         let r_pool: Result<&PgPool, async_graphql::Error> = ctx.data::<PgPool>();
 
         let Ok(pool) = r_pool else {
@@ -25,11 +28,18 @@ impl LoginQuery {
             return Err(async_graphql::Error::new("Login failed!"));
         }
 
-        match gql_auth::get_token(&token) {
-            Ok(_) => Ok(true),
+        let r_token = gql_auth::get_token(&token);
+
+        let Ok(user_claims) = r_token else {
+            error!("Error while checking the token [{}], the error is [{}]", token, r_token.err().unwrap().message);
+            return Err(async_graphql::Error::new("Token is invalid!"));
+        };
+
+        match UserModel::get_user_for_name(&user_claims.claims.name, pool).await {
+            Ok(user_model) => Ok(UserModel::convert_to_gql(&user_model)),
             Err(error) => {
-                error!("Error while checking the token [{}], the error is [{}]", token, error.message);
-                Err(async_graphql::Error::new("Token is invalid!"))
+                error!("Token is wrong [{}] the error is [{}]", token, error.to_string());
+                Err(async_graphql::Error::new("Token is wrong!"))
             }
         }
     }
